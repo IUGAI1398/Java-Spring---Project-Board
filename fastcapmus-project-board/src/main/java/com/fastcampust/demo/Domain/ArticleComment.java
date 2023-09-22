3257d54f688e704581123d86f2d70ec624aa9a46
IUGAI <iugaialeksandrr@gmail.com>
2023-09-22 17:55:16 +0900

    add comment

--- fastcapmus-project-board/src/main/java/com/fastcampust/demo/Domain/ArticleComment.java
+++ fastcapmus-project-board/src/main/java/com/fastcampust/demo/Domain/ArticleComment.java
@@ -3,11 +3,11 @@ package com.fastcampust.demo.Domain;
 import java.time.LocalDateTime;
 
 public class ArticleComment {
-    private  Long id;
-    private  Article article;
-    private  String content;
-    private LocalDateTime createdAt;
-    private  String createdby;
-    private  LocalDateTime CreatedAt;
+    private  Long id; //아이디
+    private  Article article; // 게시글 아이디
+    private  String content; // 댓글 내용
+    private LocalDateTime createdAt; // 댓글 작성 날짜
+    private  String createdby; // 댓글 작성자
+    private  LocalDateTime CreatedAt; //댓글
     private  String createdBy;
 }
