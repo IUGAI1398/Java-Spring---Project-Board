894c765c83a49c8003236a013db543a8cddbb902
IUGAI <iugaialeksandrr@gmail.com>
2023-09-22 17:25:32 +0900

    createDTO

---
+++ fastcapmus-project-board/src/main/java/com/fastcampust/demo/Domain/ArticleComment.java
@@ -0,0 +1,13 @@
+package com.fastcampust.demo.Domain;
+
+import java.time.LocalDateTime;
+
+public class ArticleComment {
+    private  Long id;
+    private  Article article;
+    private  String content;
+    private LocalDateTime createdAt;
+    private  String createdby;
+    private  LocalDateTime CreatedAt;
+    private  String createdBy;
+}
