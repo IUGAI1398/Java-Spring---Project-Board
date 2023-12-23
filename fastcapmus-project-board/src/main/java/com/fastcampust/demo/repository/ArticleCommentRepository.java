98f106cbc666b86d217b2d520f80b0921f3ece14
IUGAI1398 <hhhs2332@gmail.com>
2023-12-23 20:28:37 +0900

    initagain

--- fastcapmus-project-board/src/main/java/com/fastcampust/demo/repository/ArticleCommentRepository.java
+++ fastcapmus-project-board/src/main/java/com/fastcampust/demo/repository/ArticleCommentRepository.java
@@ -2,6 +2,8 @@ package com.fastcampust.demo.repository;
 
 import com.fastcampust.demo.Domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
 }
--- fastcapmus-project-board/src/main/java/com/fastcampust/demo/repository/ArticleRepository.java
+++ fastcapmus-project-board/src/main/java/com/fastcampust/demo/repository/ArticleRepository.java
@@ -2,7 +2,9 @@ package com.fastcampust.demo.repository;
 
 import com.fastcampust.demo.Domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 
 }
